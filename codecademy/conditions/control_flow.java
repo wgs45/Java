a8005e09d2c1538e50a528914e22817fcfd6a01a
W45 <73011833+wgs45@users.noreply.github.com>
2024-11-25 14:26:10 +0800

    Add files via upload

---
+++ codecademy/conditions/control_flow.java
@@ -0,0 +1,17 @@
+public class control_flow {
+    public static void main (String[] args) {
+        double itemCost = 30.99;
+
+        if (itemCost > 24.00) {
+            System.out.println("High value Item!");
+        }
+
+        boolean isFilled = false;
+
+        if (isFilled == true) {
+            System.out.println("Shipping");
+        } else {
+            System.out.println("Order not ready");
+        }
+    }
+}
